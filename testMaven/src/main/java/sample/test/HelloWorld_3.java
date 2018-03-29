783f6cabc311ef56a7a38bef5b4832b3df29cbfa
PremQA <qatreselle190@gmail.com>
2018-03-29 15:14:51 +0530

    sds

---
+++ testMaven/src/main/java/sample/test/HelloWorld_3.java
@@ -0,0 +1,12 @@
+package sample.test;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld_3 {
+@Test
+	public static void main(String[] args) {
+		System.out.println("Hekki");
+
+	}
+
+}
