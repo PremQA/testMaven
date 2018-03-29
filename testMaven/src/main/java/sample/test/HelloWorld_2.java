60a6635044546a1c57635fd1f11d123bea53f820
PremQA <qatreselle190@gmail.com>
2018-03-29 15:03:50 +0530

    new file in git

---
+++ testMaven/src/main/java/sample/test/HelloWorld_2.java
@@ -0,0 +1,12 @@
+package sample.test;
+
+import org.testng.annotations.Test;
+
+public class HelloWorld_2 {
+@Test
+	public static void main(String[] args) {
+		System.out.println("Hekki");
+
+	}
+
+}
